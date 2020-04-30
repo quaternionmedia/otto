449594d2454cef35f0acc96a6d24f68b7aebcb53
Harpo <harpo@harpo.me>
2020-04-30 16:24:12 -0700

    working auto slideshow generator!

--- otto/api/otto.py
+++ otto/api/otto.py
@@ -1,3 +1,62 @@
+import ffmpeg
+import numpy as np
+from subprocess import run
+from typing import List
+from seed import photoSeed
+from os import path
+
+def download(url):
+    filename = url.split('/')[-1]
+    if not path.isfile(filename):
+        run(["wget", "-N", url])
+    return filename
+
+class Otto:
+    def __init__(self,
+                photos:List=photoSeed,
+                logo=None,
+                music=None,
+                address=None,
+                price=None,
+                bedrooms=None,
+                bathrooms=None,
+                sqft=None):
+        self.photos = photos
+        self.logo = logo
+        self.music = music
+        self.address = address
+        self.price = price
+        self.bedrooms = bedrooms
+        self.bathrooms = bathrooms
+        self.sqft = sqft
+
+    # def getPhoto(self, photo):
+    #     return download(photo)
+
+    def getMusic(self, song=None):
+        download(song)
+
+    def render(self):
+        p = []
+        for i in self.photos:
+            p.append(
+                ffmpeg.input(
+                    download(i),
+                    loop=1,
+                    framerate=30
+                )
+                .zoompan(s='hd1080')
+                .filter('setsar', sar=1)
+            )
+        c = ffmpeg.concat(*[clip.trim(start_frame=0, end_frame=150) for clip in p] )
+        print('rendering', c)
+        out = ffmpeg.output(c, 'out.mp4')
+        out.run()
+
+if __name__ == '__main__':
+    v = Otto()
+    v.render()
+
 # get template from xml or csv
 
 # get photos from links from xml (max 20)
