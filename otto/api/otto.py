dc57b0ebedd5c80d0d662eeda897dfef8ff68210
Harpo <harpo@harpo.me>
2020-04-30 16:16:51 -0700

    initial requirements

---
+++ otto/api/otto.py
@@ -0,0 +1,20 @@
+# get template from xml or csv
+
+# get photos from links from xml (max 20)
+
+# get logo and display watermarked/ghosted to entire video
+
+# get music
+
+# add text at 5 seconds with address, city, price
+
+# add text from 10 seconds, each entry for 5 seconds
+    # number of bedrooms
+    # number of bathrooms
+    # square footage
+
+# rotate photos for 60/(number of photos) seconds each
+
+# add text at 50 seconds with address, city price
+
+# add text at 55 seconds with name, phone number, email, website
