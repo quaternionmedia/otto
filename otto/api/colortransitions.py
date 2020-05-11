5c564c733ed1393a43b6557129da7f06f053d879
Peter Kagstrom <peter.kagstrom@gmail.com>
2020-05-11 17:04:43 -0400

    cleaned up unused method

--- otto/api/colortransitions.py
+++ otto/api/colortransitions.py
@@ -4,22 +4,6 @@ from moviepy.editor import VideoClip, CompositeVideoClip, ColorClip
 duration = 5
 w,h = 800, 600
 
-def makeColor(
-        size, #tuple (x,y)
-        color=(1,1,1), #tuple (r,g,b)
-        position=(0,0), #tuple (x,y) from top left
-        opacity=0.5,
-        start=0,
-        duration=5
-        ):
-        return (ColorClip(size,color=color)
-                    .set_position(position)
-                    .set_opacity(opacity)
-                    .set_start(start)
-                    .set_duration(duration)
-                    .set_fps(30)
-                    )
-
 def growBox(t):
     surface = gizeh.Surface(w,h,bg_color=(0, 1, 0, 0.2))
     x = w*t*2
