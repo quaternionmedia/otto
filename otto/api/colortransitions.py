bf06564e1a4b5c46631e186947a7b24c63ed33a2
Harpo <harpo@harpo.me>
2020-05-12 13:01:26 -0700

    flyInAndGrow animation

--- otto/api/colortransitions.py
+++ otto/api/colortransitions.py
@@ -11,7 +11,23 @@ def growBox(duration=5, size=(800,600)):
         return surface.get_npimage(transparent=True)
     return gb
 
-clip = VideoClip(growBox, duration=5)
+def flyInAndGrow(duration=5, size=(1920,1080)):
+    surface = gizeh.Surface(size[0],size[1],bg_color=(0, 0, 0, 0))
+    def fiag(t):
+        w = size[0]*0.1
+        h = size[1]*0.8
+        x = size[0]/2
+        y = -size[1]/2
+        if (t < 0.5):
+            y = -h/2 + t*h
+        if (t > 0.5 and t < duration):
+            y = h/2
+            w = size[0]*(t*0.8/duration)
+        rect = gizeh.rectangle(lx=w,ly=h,xy=(x,y),fill=(0,0,.5,.4))
+        rect.draw(surface)
+
+        return surface.get_npimage()
+    return fiag
 
 # clip = VideoClip(growBox())
 #
