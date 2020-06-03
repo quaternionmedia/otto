9d2b54196c77ae16c5461e857c09f48eaaef1722
Harpo <harpo@harpo.me>
2020-06-03 13:00:09 -0700

    added long names to cli

--- otto/cliparse.py
+++ otto/cliparse.py
@@ -3,21 +3,22 @@ import argparse
 parser = argparse.ArgumentParser()
 
 #render
-parser.add_argument('-n', action='store_false', dest='render', default=True,
+parser.add_argument('--dry', '-n', action='store_false', dest='render', default=True,
                     help='for dry run (no render)')
 
 #renderFrame
-parser.add_argument('-f', action='store', type=int, dest='frame', default=-1,
+parser.add_argument('--frame', '-f', action='store', type=int, dest='frame', default=-1,
                     help='followed by a number (-f 9) to render that frame')
 
 #open
-parser.add_argument('-o', action='store_true', dest='open', default=False,
+parser.add_argument('--open', '-o', action='store_true', dest='open', default=False,
                     help='open rendered video with vlc')
 
 #verbose
-parser.add_argument('-v', action='store_true', dest='verbose', default=False,
+parser.add_argument('--verbose', '-v', action='store_true', dest='verbose', default=False,
                     help='for verbose')
 
-parser.add_argument('--size', dest='size', nargs='+', type=int, 
+parser.add_argument('--size', '-s', dest='size', nargs='+', type=int, 
                     help='size of rendered movie')
+
 args = parser.parse_args()
