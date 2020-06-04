2863ee46dec4332c8a58f534a8b08c13cf189e5f
Harpo <harpo@harpo.me>
2020-06-04 09:49:17 -0700

    split VideoForm into models.py

--- otto/main.py
+++ otto/main.py
@@ -1,30 +1,12 @@
 import render
 import os, json
-from typing import List
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, Response, JSONResponse, FileResponse
 from fastapi.encoders import jsonable_encoder
 from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel, AnyUrl
 from getdata import urlToJson
 from uvicorn import run
 
-class VideoForm(BaseModel):
-    NAME: str
-    LOGO: str #AnyUrl
-    ADDRESS: str
-    PHONE: str
-    HOURS: str
-    WEBSITE: str #AnyUrl = "talahairstudio.com"
-    INITIAL: str
-    BULLETS: str
-    OPTIONAL: str = ""
-    MEDIA: List[str] = ["videos/talavid.mp4", "audios/talaaudio.mp3"]
-    CALL: str
-    CLOSING: str
-    FONTCOLOR: str = "#FFFFFF"
-    THEMECOLOR: str = "#CC5500"
-    FONT: str = "Segoe_UI_Bold"
 
 app = FastAPI()
 # app.mount("/static", StaticFiles(directory='static', html=True), name="static")
