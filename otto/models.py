02b2b3c39327cc0a9802870f2c82d268c66e63e5
Harpo <harpo@harpo.me>
2020-06-06 11:34:00 -0700

    added default '' to VideoForm fields

--- otto/models.py
+++ otto/models.py
@@ -3,17 +3,18 @@ from typing import List
 
 class VideoForm(BaseModel):
     NAME: str
-    LOGO: str #AnyUrl
-    ADDRESS: str
-    PHONE: str
-    HOURS: str
-    WEBSITE: str #AnyUrl = "talahairstudio.com"
-    INITIAL: str
-    BULLETS: str
-    OPTIONAL: str = ""
-    MEDIA: List[str] = ["videos/talavid.mp4", "audios/talaaudio.mp3"]
-    CALL: str
-    CLOSING: str
-    FONTCOLOR: str = "#FFFFFF"
-    THEMECOLOR: str = "#CC5500"
-    FONT: str = "Segoe_UI_Bold"
+    LOGO: str = '' #AnyUrl
+    ADDRESS: str = ''
+    PHONE: str = ''
+    HOURS: str = ''
+    WEBSITE: str = '' #AnyUrl = 'talahairstudio.com'
+    INITIAL: str = ''
+    BULLETS: str = ''
+    OPTIONAL: str = ''
+    MEDIA: List[str] = ['videos/talavid.mp4', 'audios/talaaudio.mp3']
+    CALL: str = ''
+    CLOSING: str = ''
+    FONTCOLOR: str = '#FFFFFF'
+    THEMECOLOR: str = '#CC5500'
+    FONT: str = 'Segoe_UI_Bold'
+
