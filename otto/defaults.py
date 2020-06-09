6252e93c75bf9737e8c50449a3c69855570503d2
Harpo <harpo@harpo.me>
2020-06-09 13:18:30 -0700

    added default fields for templates

--- otto/defaults.py
+++ otto/defaults.py
@@ -1,4 +1,8 @@
 defaults = {
     'FONTCOLOR': '#FFFFFF',
-    'THEMECOLOR': '#FFFF00'
+    'THEMECOLOR': '#FFFF00',
+    'ADDRESS': '123 Main St',
+    'HOURS': '24/7',
+    'WEBSITE': 'alfred.quaternion.media',
+    'PHONE': '916.674.2776'
 }
