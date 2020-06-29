ef4189d16679a8903d3e15769899251f8cb0119d
Harpo <harpo@harpo.me>
2020-06-29 12:57:28 -0700

    fade out audio and video from final clip

--- otto/render.py
+++ otto/render.py
@@ -15,7 +15,7 @@ def renderEdl(edl, media, audio=None, filename='render.mp4', moviesize=(1920,108
             tmp = getattr(templates, clip['name'])
             print('making template', tmp )
             clips.append(
-                tmp(**clip['data'], duration=clip['duration'], moviesize=moviesize)
+                tmp(**clip['data'], duration=clip['duration'], clipsize=moviesize)
             )
         elif clip['type'] == 'video':
             clips.append(
@@ -24,7 +24,7 @@ def renderEdl(edl, media, audio=None, filename='render.mp4', moviesize=(1920,108
                 .set_duration(clip['duration'])
             )
     print('made clips', clips)
-    video = concatenate_videoclips(clips).resize(moviesize).crossfadeout(1)
+    video = concatenate_videoclips(clips).resize(moviesize)
     print('made video', video)
     kburns(media, moviesize=moviesize, filename=f'{filename}_kbout.mp4')
     slides = (VideoFileClip(f'{filename}_kbout.mp4')
@@ -34,7 +34,7 @@ def renderEdl(edl, media, audio=None, filename='render.mp4', moviesize=(1920,108
     video = CompositeVideoClip([slides, video])
     if audio:
         video = video.set_audio(AudioFileClip(audio))
-    video = video.set_duration(duration)
+    video = video.set_duration(duration).crossfadeout(1).audio_fadeout(1)
     video.write_videofile(filename, fps=30, logger=logger, threads=8)
 
 def renderForm(form, filename='render.mp4', logger='bar'):
