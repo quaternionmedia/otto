a184813847227cf19ef2735e86f75d21d819faa6
Harpo <harpo@harpo.me>
2020-07-29 15:09:47 -0700

    working renderMultitrack() !!!

--- otto/render.py
+++ otto/render.py
@@ -38,6 +38,36 @@ def renderEdl(edl, media, audio=None, filename='render.mp4', moviesize=(1920,108
     video = video.set_duration(duration).crossfadeout(1).audio_fadeout(1)
     video.write_videofile(filename, fps=30, logger=logger, threads=8)
 
+def renderMultitrack(edl, audio=None, filename='render.mp4', moviesize=(1920,1080), logger='bar'):
+    clips = []
+    for clip in edl:
+        print('making clip', clip, type(clip))
+        if clip['type'] == 'template':
+            tmp = getattr(templates, clip['name'])
+            print('making template', tmp )
+            clips.append(
+                tmp(**clip['data'], duration=clip['duration'], clipsize=moviesize).set_start(clip['start'])
+            )
+        elif clip['type'] == 'video':
+            c = (
+                VideoFileClip(clip['name'], target_resolution=(moviesize[1], moviesize[0]))
+                .subclip(clip.get('inpoint', 0))
+                .crossfadein(1)
+                # .crossfadeout(1)
+                # .set_duration(getattr(clip, 'duration', ))
+                # .set_start(clip['start'])
+            )
+            if clip.get('duration'):
+                c = c.set_duration(clip['duration'])
+            clips.append(c.crossfadeout(1))
+    print('made clips', clips)
+    video = CompositeVideoClip(clips)
+    print('made video', video)
+    if audio:
+        video = video.set_audio(AudioFileClip(audio))
+    video = video.crossfadeout(1).audio_fadeout(1)
+    video.write_videofile(filename, fps=30, logger=logger, threads=8)
+
 def renderForm(form, filename='render.mp4', logger='bar'):
     v = Otto(form)
     v.render().write_videofile(filename=filename, fps=30, logger=logger)
