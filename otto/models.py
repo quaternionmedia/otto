84a64cb1c6001e60bde1d31aa2527545adda2ff0
Harpo <harpo@harpo.me>
2021-11-15 19:23:59 -0800

    defined Edl and Clip models with Pydantic

--- otto/models.py
+++ otto/models.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel, AnyUrl
-from typing import List, Dict
+from typing import List, Dict, Optional, Union
 from otto.as_form import as_form
 
 @as_form
@@ -22,7 +22,21 @@ class VideoForm(BaseModel):
     font: str = 'Segoe_UI_Bold'
     duration: float = 5
 
+class Clip(BaseModel):
+    duration: float = 5
+    type: Optional[str] = None
+    name: Optional[str] = None
+    inpoint: Optional[float] = None
+    outpoint: Optional[float] = None
+    offset: Optional[float] = None
+    start: Optional[float] = None
+    position: Optional[tuple] = None
+    data: Optional[dict] = None
+    resize: Optional[Union[float, tuple]]
+    relative: Optional[bool] = True
+    fxs: Optional[dict] = None
+
 
 class Edl(BaseModel):
-    clips: List
+    clips: List[Clip]
     duration: float = None
