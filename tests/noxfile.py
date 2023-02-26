a5b02b3e2d4b58eca5f302c340e52dcae5f3f927
Harpo <harpo@harpo.me>
2023-02-26 12:33:17 -0800

    Add nox tests, based on alfred tests

---
+++ tests/noxfile.py
@@ -0,0 +1,27 @@
+import nox
+
+
+@nox.session
+def blacken(session):
+    session.install("black")
+    session.run("black", "-S", "-v", ".")
+
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "-v", ".")
+
+
+@nox.session(tags=["test"])
+def coverage(session):
+    session.install("-r", "../requirements.txt")
+    session.install("-r", "requirements-tests.txt")
+    session.install("-e", "..")
+    session.run(
+        "pytest",
+        "-vv",
+        "--timeout=600",
+        "-n",
+        "auto",
+    )
