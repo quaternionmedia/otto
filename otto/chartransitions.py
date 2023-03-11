fa774456e130d6a7b67e889a3b3c3b7e33dcc094
Harpo <harpo@harpo.me>
2023-03-11 15:31:06 -0800

    Fix chartransitions

--- otto/chartransitions.py
+++ otto/chartransitions.py
@@ -1,46 +1,47 @@
 import numpy as np
 
-from moviepy.editor import *
-from moviepy.video.tools.segmenting import findObjects
 
-# WE CREATE THE TEXT THAT IS GOING TO MOVE, WE CENTER IT.
-
-screensize = (1920, 1080)
-txtClip = TextClip(
-    "Steve's Auto Care", color='white', font="Amiri-Bold", kerning=5, fontsize=81
-)
-cvc = CompositeVideoClip([txtClip.set_pos('center')], size=screensize)
-
-# THE NEXT FOUR FUNCTIONS DEFINE FOUR WAYS OF MOVING THE LETTERS
+def rotMatrix(a):
+    return np.array([[np.cos(a), np.sin(a)], [-np.sin(a), np.cos(a)]])
 
 
-# helper function
-rotMatrix = lambda a: np.array([[np.cos(a), np.sin(a)], [-np.sin(a), np.cos(a)]])
+def damping(t):
+    return 1.0 / (0.3 + t**8)
 
 
 def vortex(screenpos, i, nletters):
-    d = lambda t: 1.0 / (0.3 + t**8)  # damping
     a = i * np.pi / nletters  # angle of the movement
     v = rotMatrix(a).dot([-1, 0])
     if i % 2:
         v[1] = -v[1]
-    return lambda t: screenpos + 400 * d(t) * rotMatrix(0.5 * d(t) * a).dot(v)
+    return lambda t: screenpos + 400 * damping(t) * rotMatrix(0.5 * damping(t) * a).dot(
+        v
+    )
 
 
-def cascade(screenpos, i, nletters):
+def cascade(screenpos, i):
     v = np.array([0, -1])
-    d = lambda t: 1 if t < 0 else abs(np.sinc(t) / (1 + t**4))
+
+    def d(t):
+        return 1 if t < 0 else abs(np.sinc(t) / (1 + t**4))
+
     return lambda t: screenpos + v * 400 * d(t - 0.15 * i)
 
 
-def arrive(screenpos, i, nletters):
+def arrive(screenpos, i):
     v = np.array([-1, 0])
-    d = lambda t: max(0, 3 - 3 * t)
+
+    def d(t):
+        return max(0, 3 - 3 * t)
+
     return lambda t: screenpos - 400 * v * d(t - 0.2 * i)
 
 
 def vortexout(screenpos, i, nletters):
-    d = lambda t: max(0, t)  # damping
+    def d(t):
+        # damping
+        return max(0, t)
+
     a = i * np.pi / nletters  # angle of the movement
     v = rotMatrix(a).dot([-1, 0])
     if i % 2:
@@ -50,27 +51,8 @@ def vortexout(screenpos, i, nletters):
     )
 
 
-# WE USE THE PLUGIN findObjects TO LOCATE AND SEPARATE EACH LETTER
-
-letters = findObjects(cvc)  # a list of ImageClips
-
-
-# WE ANIMATE THE LETTERS
-
-
 def moveLetters(letters, funcpos):
     return [
         letter.set_pos(funcpos(letter.screenpos, i, len(letters)))
         for i, letter in enumerate(letters)
     ]
-
-
-clips = [
-    CompositeVideoClip(moveLetters(letters, funcpos), size=screensize).subclip(0, 5)
-    for funcpos in [vortex, cascade, arrive, vortexout]
-]
-
-# WE CONCATENATE EVERYTHING AND WRITE TO A FILE
-
-final_clip = concatenate_videoclips(clips)
-final_clip.write_videofile('text.mp4', fps=30, codec='mpeg4')
