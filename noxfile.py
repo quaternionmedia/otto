20b8f862ce90da8c2446b13ce7175bf0ffc1e8dc
Harpo <harpo@harpo.me>
2023-04-03 15:53:18 -0700

    Adds pytest xdist to test requirements

--- noxfile.py
+++ noxfile.py
@@ -3,24 +3,21 @@ import nox
 
 @nox.session
 def blacken(session):
-    session.install("black")
+    session.install('black')
     if session.posargs and session.posargs[0] == 'check':
-        session.run("black", "-S", "-v", ".", "--check")
+        session.run('black', '-S', '-v', '.', '--check')
     else:
-        session.run("black", "-S", "-v", ".")
+        session.run('black', '-S', '-v', '.')
 
 
 @nox.session
 def lint(session):
-    session.install("flake8")
-    session.run("flake8", "-v", ".")
+    session.install('flake8')
+    session.run('flake8', '-v', '.')
 
 
-@nox.session(tags=["test"])
+@nox.session(tags=['test'])
 def coverage(session):
-    session.install(".[test,render]")
-    session.run("mkdir", "-p", "data", external=True)
-    session.run(
-        "pytest",
-        "-vv"
-    )
+    session.install('.[test,render]')
+    session.run('mkdir', '-p', 'data', external=True)
+    session.run('pytest', '-vv', '-n', 'auto')
