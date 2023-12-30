c2017751e2acb624c3fd3f11f51cac4273bf81df
Harpo <harpo@harpo.me>
2023-12-29 22:43:54 -0800

    better download error handling

--- otto/getdata.py
+++ otto/getdata.py
@@ -1,46 +1,47 @@
-from os import path
 from csv import reader
-from json import loads
-from time import strftime
-from requests import head
 from hashlib import sha256
+from json import loads
+from os import path
+from time import strftime
+
+from requests import head
+
 from otto.utils import download_url
 
 content_types = ['image/jpeg', 'video/mp4', 'image/png', 'audio/mpeg']
 extensions = ['jpg', 'mp4', 'png', 'mp3']
 
 
-def hash(s):
+def sha(s):
     return sha256(s.encode()).hexdigest()
 
 
 def download(url, location='data'):
     if not url.startswith('http'):
         return url
-    else:
-        try:
-            h = head(url, allow_redirects=True)
-            content_type = h.headers.get('content-type').lower()
-            if not content_type:
-                raise Exception('no content_type')
-            if 'text' in content_type:
-                raise Exception('text not allowed')
-            if 'html' in content_type:
-                raise Exception('html not allowed')
-            if float(h.headers.get('content-length')) > 1e10:
-                raise Exception('filesize too large', h.headers.get('content-length'))
-            ct = h.headers.get('content-type')
-            if ct not in content_types:
-                raise Exception('type not allowed', h.headers.get('content-type'))
-            ext = extensions[content_types.index(ct)]
-            basename = f'{hash(url)}.{ext}'
-            filename = path.join(location, basename) if location else basename
-            if not path.isfile(filename):
-                download_url(url, filename)
-
-            return filename
-        except Exception as e:
-            print('error downloading file', url, e)
+    h = head(url, allow_redirects=True)
+    content_type = h.headers.get('content-type').lower()
+    if not content_type:
+        raise Exception('no content_type')
+    if 'text' in content_type:
+        raise Exception('text not allowed')
+    if 'html' in content_type:
+        raise Exception('html not allowed')
+    if float(h.headers.get('content-length')) > 1e10:
+        raise Exception('filesize too large', h.headers.get('content-length'))
+    ct = h.headers.get('content-type')
+    if ct not in content_types:
+        raise Exception('type not allowed', h.headers.get('content-type'))
+    ext = extensions[content_types.index(ct)]
+    basename = f'{sha(url)}.{ext}'
+    filename = path.join(location, basename) if location else basename
+    if path.isfile(filename):
+        return filename
+    try:
+        download_url(url, filename)
+        return filename
+    except Exception as e:
+        print('error downloading file', url, e)
 
 
 def openCsv(path):
